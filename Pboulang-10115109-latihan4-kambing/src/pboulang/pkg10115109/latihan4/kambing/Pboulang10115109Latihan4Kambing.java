f64f8cdbab99f1e865f7eca8031c9b9c3c922cc8
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:13:10 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan4-kambing/src/pboulang/pkg10115109/latihan4/kambing/Pboulang10115109Latihan4Kambing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan4.kambing;
+
+
+public class Pboulang10115109Latihan4Kambing {
+    public void tambahKambing(){
+    //deklarasi variabel lokal
+    int jumlahKambing = 0;
+    
+    jumlahKambing = jumlahKambing + 5;
+   System.out.println("jumlah kambing setelah ditambah:" +jumlahKambing);
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pboulang10115109Latihan4Kambing kambingJantan = new Pboulang10115109Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
